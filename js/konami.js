3df1dae6e7607061d309f8b077aa00154d01feb6
Joe Beltran <jab102191@gmail.com>
2023-03-22 10:58:07 -0500

    add ajax exercise

--- js/konami.js
+++ js/konami.js
@@ -2,9 +2,21 @@
 
 let key = [];
 let konamiCode = [38,38,40,40,37,39,37,39,66,65,13];
+let $renderInput = $(`#codeInput`)
 $(document).keyup(function(event){
 	key = key.concat([event.keyCode]);
 	if (key.toString() === konamiCode.toString()) {
 		alert(`You have added 30 lives!`);
 	}
+});
+
+$(document).keyup(function(event){
+	let pressedKey = (event.keyCode);
+	let up = `<img src="images/Loteria/konami-code-up.png" alt="arrow">`;
+	console.log(pressedKey)
+	if (pressedKey === 38 || 40 || 37 || 39) {
+		$renderInput.innerHTML = up
+		console.log($renderInput.innerHTML)
+	}
+	return $renderInput.innerHTML
 });
--- js/requests-responses-lesson.js
+++ js/requests-responses-lesson.js
@@ -4,23 +4,36 @@
 // 	email: "jab102191@gmail.com"
 // }
 
-const getMovie = () => {
-	fetch(`https://swapi.dev/api/films/1/`)
-		.then(response => response.json())
-		.then(data => {
-			return(data);
-		}).catch(error => {
-			console.log(error);
-	});
-}
+// const getMovie = () => {
+// 	fetch(`https://swapi.dev/api/films/1/`)
+// 		.then(response => response.json())
+// 		.then(data => {
+// 			return(data);
+// 		}).catch(error => {
+// 			console.log(error);
+// 	});
+// }
+//
+// async function getMovieAsync(){
+// 	try {
+// 		let response = await fetch(`https://swapi.dev/api/films/1/`)
+// 		let data = await response.json();
+// 		console.log(data);
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// }
 
-async function getMovieAsync(){
+const getPerson = async (person= 1 ) => {
 	try {
-		let response = await fetch(`https://swapi.dev/api/films/1/`)
-		let data = await response.json();
-		console.log(data);
+		let response = await fetch(`https://swapi.dev/api/people/${person}/`);
+		return await response.json();
 	} catch (error) {
 		console.log(error);
 	}
 }
 
+(async ()=>{
+	let personData = await getPerson(4);
+	console.log(personData)
+})();
